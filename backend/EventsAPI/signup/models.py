34d2f84735f61589169a123949daf27764041290
Sefakor <sefakor1105@gmail.com>
2020-09-08 22:21:13 +0000

    changes

--- backend/EventsAPI/signup/models.py
+++ backend/EventsAPI/signup/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 
+#@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+#def create_auth_token(sender, instance=None, created=False, **kwargs):
+    #if created:
+        #Token.objects.create(user=instance)
+
+
+
 # Create your models here.
 
 class user(models.Model):
