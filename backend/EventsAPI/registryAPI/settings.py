72d3f36417bba186077c1d0ced3b1f099c065ec8
Sefakor <sefakor1105@gmail.com>
2020-09-08 09:58:10 +0000

    update commit messages

--- backend/EventsAPI/registryAPI/settings.py
+++ backend/EventsAPI/registryAPI/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ukr&+45r9y_2fa@i$#ua(ezjg&)w$rdt@0*zki0cu&!#in!6_+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '13.89.37.205']
 
 
 # Application definition
