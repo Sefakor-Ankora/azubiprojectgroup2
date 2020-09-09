5f99e29b39b8a3af46db8abf9f38216d36588fd0
Sefakor <sefakor1105@gmail.com>
2020-09-09 10:44:18 +0000

    replace manage.py

--- backend/EventsAPI/manage.py
+++ backend/EventsAPI/manage.py
@@ -5,7 +5,6 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'registryAPI.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,7 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )from exc
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
