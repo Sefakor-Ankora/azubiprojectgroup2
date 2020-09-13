9feac7356c2b952b4f935c244b51ea8ecb63360a
Sefakor <sefakor1105@gmail.com>
2020-09-13 21:20:55 +0000

    add unit tests for models and serializers

--- backend/EventsAPI/events/tests.py
+++ backend/EventsAPI/events/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from factory import DjangoModelFactory, Faker
+
+from ..models import Events
+
 
 # Create your tests here.
+class EventsFactory(DjangoModelFactory):
+    speaker_name = Faker('name')
+    topic = Faker('topic')
+    time_scheduled = Faker('time_scheduled')
+    time = Faker('time')
+    room_capacity = Faker('room_capacity')
+    description = Faker('description')
+
+    
+
+    class Meta:
+        model = Events
+
+
+
+
+# tests/test_models.py
+from django.test import TestCase
+
+from ..models import Events
+from .factories import EventsFactory
+
+
+class CompanyTestCase(TestCase):
+    def test_str(self):
+        """Test for string representation."""
+        events = EventsFactory()
+        self.assertEqual(str(events), events.name)
+
+
+
+# tests/test_serializers.py
+from django.test import TestCase
+
+from ..serializers import CompanySerializer
+from .factories import CompanyFactory
+
+
+class EventsSerializer(TestCase):
+    def test_model_fields(self):
+        """Serializer data matches the Company object for each field."""
+        events = EventsFactory()
+        for field_name in [
+            'id', 'speaker_name', 'topic', 'time_scheduled', 'time', 'room_capacity',
+            'description'
+        ]:
+            self.assertEqual(
+                serializer.data[field_name],
+                getattr(events, field_name)
--- backend/EventsAPI/register/tests.py
+++ backend/EventsAPI/register/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from factory import DjangoModelFactory, Faker
+
+from ..models import Register
 
 # Create your tests here.
+class RegisterFactory(DjangoModelFactory):
+    fullname = Faker('fullname')
+    email = Faker('email')
+    phonenumber = Faker('phonenumber')
+    ticketnumber = Faker('ticketnumber')
+    
+
+    class Meta:
+        model = Register
+
+
+
+
+# tests/test_models.py
+from django.test import TestCase
+
+from ..models import Register
+from .factories import RegisterFactory
+
+
+class CompanyTestCase(TestCase):
+    def test_str(self):
+        """Test for string representation."""
+        register = RegisterFactory()
+        self.assertEqual(str(register), register.name)
+
+
+
+
+
+# tests/test_serializers.py
+from django.test import TestCase
+
+from ..serializers import RegisterSerializer
+from .factories import RegisterFactory
+
+
+class RegisterSerializer(TestCase):
+    def test_model_fields(self):
+        """Serializer data matches the Company object for each field."""
+        register = RegisterFactory()
+        for field_name in [
+            'id', 'fullname', 'email', 'phonenumber', 'ticketnumber'
+        ]:
+            self.assertEqual(
+                serializer.data[field_name],
+                getattr(register, field_name)
