787164cdfbfa7d5b7fc1fb8c0cc25b16ad9c4cdd
Dolly33 <kpobidolly@yahoo.com>
2020-09-17 11:35:52 +0000

    event listings update

--- frontend/src/components/Login/Details.js
+++ frontend/src/components/Login/Details.js
@@ -1,37 +1,56 @@
-import React from 'react'
-import Container from '@material-ui/core/Container';
-import 'tachyons';
-import './Details.css';
-import Footer from './Footer.js';
-
-
+import React from "react";
+import Container from "@material-ui/core/Container";
+import "tachyons";
+import "./Details.css";
+import Footer from "./Footer.js";
 
 export default function Details() {
-    return (
-        
-            <Container maxWidth='xl'>
-            <nav class="flex justify-between flex-grow bb b--black-10">
-            <a class="f15 dib black no-underline pv2 ph4 br-pill flex items-center " href="/">Evently
-            </a>
-            <div class="flex-grow pa3 flex items-center">
-                <a class="f6 link dib black dim hover-white mr3 mr4-ns" href="#0">Link 1</a>
-                <a class="f6 link dib black dim hover-white mr3 mr4-ns" href="#0">Browse Events</a>
-                <a class="f6 dib black bg-animate hover-bg-black hover-white no-underline pv2 ph4 br-pill ba b--white-20" href="#0">Sign In</a>
-            </div>
-            </nav>
-            <nav class="subtitle">
-            <div class="nowrap overflow-x-auto">
-             <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 1">All</a>
-            <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 2">Today</a>
-            <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 3">Virtual</a>
-             <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 4">Free</a>
-             <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 4">Paid</a>
+  return (
+    <Container maxWidth="xl">
+      <nav class="flex justify-between flex-grow bb b--black-10">
+        <a
+          class="f15 dib black no-underline pv2 ph4 br-pill flex items-center "
+          href="/"
+        >
+          Evently
+        </a>
+        <div class="flex-grow pa3 flex items-center">
+          <a class="f6 link dib black dim hover-white mr3 mr4-ns" href="#0">
+            Link 1
+          </a>
+          <a class="f6 link dib black dim hover-white mr3 mr4-ns" href="#0">
+            Browse Events
+          </a>
+          <a
+            class="f6 dib black bg-animate hover-bg-black hover-white no-underline pv2 ph4 br-pill ba b--white-20"
+            href="#0"
+          >
+            Sign In
+          </a>
+        </div>
+      </nav>
+      <nav class="subtitle">
+        <div class="nowrap overflow-x-auto">
+          <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 1">
+            All
+          </a>
+          <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 2">
+            Today
+          </a>
+          <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 3">
+            Virtual
+          </a>
+          <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 4">
+            Free
+          </a>
+          <a class="link dim black f5 f4-ns dib mr3" href="/" title="Link 4">
+            Paid
+          </a>
+        </div>
+      </nav>
 
-             </div>
-            </nav>
-
-            <div>
-let imgList = [
+      <div>
+          let imgList = [
   {
     img: "/images/1.png",
     date: "Mon 14th Sept,2020",
@@ -116,16 +135,12 @@ const Gallery = ({ images }) => {
   );
 };
 
-render(<Gallery images={imgList} />, document.getElementById("root"));
+render(<Gallery images={imgList} />);
+</div>
 
-            </div>
-            <div>
-                
-            </div>
-            <div>
-                <Footer/>
-            </div>
-            </Container>
-        
-    )
-}
+      <div>
+        <Footer />
+      </div>
+    </Container>
+  );
+};
