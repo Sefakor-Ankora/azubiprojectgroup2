857d5eb5fae4acfcec08e4e2d2212d4a7cb55c48
Dolly33 <kpobidolly@yahoo.com>
2020-09-18 06:50:03 +0000

    dashboard style update

--- frontend/src/components/Login/Dashboard.js
+++ frontend/src/components/Login/Dashboard.js
@@ -10,43 +10,45 @@ export default function Dashboard() {
     return (
         <div>
             <div>
-                <Navbar/>
+                <Navbar />
             </div>
             <div className="dashboard">
 
                 <div className="myAccount">
-                    <h2>My Account</h2>
+                    <h3>My Account</h3>
                     <p>Browse Events</p>
                     <p>Registered Events</p>
                     <Link to="/">Log Out</Link>
                 </div>
 
                 <div className="personalDetails">
-                    <div>
-                        <h2>Account Details</h2>
-                        <p>FirstName LastName</p>
-                        <p>name@example.com</p>
-                        <p><a href="https://azubigroup2.azurewebsites.net/">Change Password</a></p>
-                    </div>
 
-                    {/* <div>
-                        <h2>Address Book</h2>
-                        <p>Address Line 1
-                        Address Line 2
-                        Address Line 3
-                        </p>
-                    </div>
+                    <div className="personalDetails2">
+                        <div className="accountDetails">
+                            <h3>Account Details</h3>
+                            <p>FirstName LastName</p>
+                            <p>name@example.com</p>
+                            <p><a href="https://azubigroup2.azurewebsites.net/">Change Password</a></p>
+                        </div>
 
-                    <div>
-                        <h2>NewsLetter Preferences</h2>
+                        <div className="addressBook">
+                            <h3>Address Book</h3>
+                            <p>Address Line 1 </p>
+                            <p>Address Line 2 </p>
+                            <p>Address Line 3 </p>
+
+                        </div>
+                    </div>
+                    <div className="newsletter">
+                        <h3>NewsLetter Preferences</h3>
                         <p>You are currently not subscribed to our newsletter</p>
                         <p><a href="https://azubigroup2.azurewebsites.net/">Edit Preferences</a></p>
-                    </div> */}
+                    </div>
                 </div>
 
             </div>
             <div>
-                <Footer/>
+                <Footer />
             </div>
         </div>
 
